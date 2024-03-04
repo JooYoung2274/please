816e7d8c0d795af806f2877c0410d4028b7be55d
JooYoung2274 <jooooo2274@gmail.com>
2024-03-04 11:14:29 +0900

    서버쪽에서 api 조회

--- main/background.ts
+++ main/background.ts
@@ -3,8 +3,15 @@ import { app, ipcMain } from 'electron';
 import serve from 'electron-serve';
 import { createWindow } from './helpers';
 import * as fs from 'fs';
+import axios from 'axios';
 
 const isProd = process.env.NODE_ENV === 'production';
+
+const currentPrice = {
+  'KRW-BTC': 0,
+  'KRW-ETH': 0,
+  'KRW-XRP': 0,
+};
 
 if (isProd) {
   serve({ directory: 'app' });
@@ -19,6 +26,13 @@ if (isProd) {
     width: 1000,
     height: 600,
   });
+
+  // setInterval(async () => {
+  //   const { data } = await axios.get('https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-XRP');
+  //   currentPrice['KRW-BTC'] = data[0].trade_price;
+  //   currentPrice['KRW-ETH'] = data[1].trade_price;
+  //   currentPrice['KRW-XRP'] = data[2].trade_price;
+  // }, 1000);
 
   if (isProd) {
     await mainWindow.loadURL('app://./home');
@@ -38,8 +52,6 @@ ipcMain.on('message', async (event, arg) => {
 });
 
 ipcMain.on('saveFile', (evt, arg) => {
-  console.log(arg);
-
   if (arg.accessKey === '' || arg.secretKey === '') {
     evt.sender.send('reply', { status: 'fail' });
     return;
@@ -63,8 +75,7 @@ ipcMain.on('getSavedUserDataFile', (evt, arg) => {
   evt.sender.send('reply', { status: 'success', userData: JSON.parse(userData) });
 });
 
-ipcMain.on('order', (evt, arg) => {
-  console.log(arg);
+ipcMain.on('orderFirst', (evt, arg) => {
   const dataFilePath = `${__dirname}/assets_data.json`;
   const assetsData = JSON.stringify(arg, null, 2);
   fs.writeFileSync(dataFilePath, assetsData, 'utf8');
@@ -72,12 +83,31 @@ ipcMain.on('order', (evt, arg) => {
 
 ipcMain.on('getSavedAssetsDataFile', (evt, arg) => {
   const dataFilePath = `${__dirname}/assets_data.json`;
-
   const assetsData = fs.readFileSync(dataFilePath, 'utf8');
-  console.log(assetsData);
+
   if (assetsData === '') {
     evt.sender.send('assetsReturn', { status: 'fail' });
     return;
   }
   evt.sender.send('assetsReturn', { status: 'success', assetsData: JSON.parse(assetsData) });
+});
+
+ipcMain.on('getSavedReservationOrderDataFile', (evt, arg) => {
+  const dataFilePath = `${__dirname}/reservation_order_data.json`;
+
+  const reservationOrderData = fs.readFileSync(dataFilePath, 'utf8');
+  if (reservationOrderData === '') {
+    evt.sender.send('reservationOrderReturn', { status: 'fail' });
+    return;
+  }
+  evt.sender.send('reservationOrderReturn', {
+    status: 'success',
+    reservationOrderData: JSON.parse(reservationOrderData),
+  });
+});
+
+ipcMain.on('orderReservation', (evt, arg) => {
+  const dataFilePath = `${__dirname}/reservation_order_data.json`;
+  const reservationOrderData = JSON.stringify(arg, null, 2);
+  fs.writeFileSync(dataFilePath, reservationOrderData, 'utf8');
 });
