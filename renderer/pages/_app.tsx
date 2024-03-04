7ccf574bea14066d20a57e37c7b0a90cbe5cd3cd
JooYoung2274 <jooooo2274@gmail.com>
2024-03-04 11:15:01 +0900

    레이아웃 추가

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,31 +1,17 @@
-import electron from 'electron';
-
 import React from 'react';
 import '../styles/global.scss';
-import Head from 'next/head';
-import Link from 'next/link';
-import Input from 'antd/lib/input/Input';
-import { Layout, Button, Select } from 'antd';
-import Home from './home';
 
-const ipcRenderer = electron.ipcRenderer;
-const { Header, Content } = Layout;
+import { RecoilRoot } from 'recoil';
+import RootLayout from '../layout/layout';
+
 function MyApp({ Component, pageProps }) {
-  const test = () => {
-    ipcRenderer.send('saveFile', '클라이언트가 보냄');
-    ipcRenderer.on('reply', (evt, arg) => {
-      console.log(arg);
-    });
-  };
-
   return (
     <React.Fragment>
-      <Head>
-        <title>테에스으트으</title>
-      </Head>
-      <Content>
-        <Component {...pageProps} />
-      </Content>
+      <RecoilRoot>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </RecoilRoot>
     </React.Fragment>
   );
 }
